e8d3c644a0b3b28feaac6525f080a7d3d5591980
dodi alfarisy <123200011@student.upnyk.ac.id>
2022-11-29 14:45:33 +0700

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/home/home.module.ts 	new file:   src/app/home/home/home.component.css 	new file:   src/app/home/home/home.component.html 	new file:   src/app/home/home/home.component.spec.ts 	new file:   src/app/home/home/home.component.ts 	modified:   src/app/login/login-routing.module.ts 	modified:   src/app/login/login.module.ts 	new file:   src/app/login/login/login.component.css 	new file:   src/app/login/login/login.component.html 	new file:   src/app/login/login/login.component.spec.ts 	new file:   src/app/login/login/login.component.ts

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
