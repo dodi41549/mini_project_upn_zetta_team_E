1b8db3819f27a0907d8f3b315c97d237b38697ee
dodi alfarisy <123200011@student.upnyk.ac.id>
2022-11-29 15:05:55 +0700

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/cart/cart.module.ts 	new file:   src/app/cart/cart/cart.component.css 	new file:   src/app/cart/cart/cart.component.html 	new file:   src/app/cart/cart/cart.component.spec.ts 	new file:   src/app/cart/cart/cart.component.ts 	modified:   src/app/home/home-routing.module.ts 	modified:   src/app/home/home/home.component.html 	modified:   src/app/menu/menu.module.ts 	new file:   src/app/menu/menu/menu.component.css 	new file:   src/app/menu/menu/menu.component.html 	new file:   src/app/menu/menu/menu.component.spec.ts 	new file:   src/app/menu/menu/menu.component.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'login',
 loadChildren:()=>import('./login/login.module').then(m => m.LoginModule)
+},
+{path:'home',
+loadChildren:()=>import('./home/home.module').then(m => m.HomeModule)
 }
 ];
 
