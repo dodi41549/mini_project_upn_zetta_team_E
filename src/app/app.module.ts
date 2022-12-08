ce7065939be54a03b28f9f8cc39ff67d174bc937
dodi alfarisy <123200011@student.upnyk.ac.id>
2022-12-08 14:13:47 +0700

    modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular'
-import { HttpLink } from 'apollo-angular/http'
-import { InMemoryCache } from '@apollo/client/core'
+import { ApolloModule } from 'apollo-angular'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -93,20 +91,7 @@ import { FormsModule } from '@angular/forms';
     MatButtonModule,
     FormsModule
   ],
-  providers: [AuthService, AuthGuard, SourceService,
-    {
-      provide : APOLLO_OPTIONS,
-      useFactory(httpLink: HttpLink) {
-        return {
-          caches: new InMemoryCache(),
-          link: httpLink.create({
-            uri: 'https://48p1r2roz4.sse.codesandbox.io'
-          })
-        }
-      },
-      deps: [HttpLink]
-    }
-  ],
+  providers: [AuthService, AuthGuard, SourceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
